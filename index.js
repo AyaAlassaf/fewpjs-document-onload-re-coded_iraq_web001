95044748c197dfaa0d175a48363cdb80e878e943
Aya Alassaf <ayafaisal242@yahoo.com>
2020-05-29 21:22:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
 // Your code goes here
 document.addEventListener("DOMContentLoaded", function() {
-  let partext=document.querySelector('p').innerHtml="This is really cool!"
+  let partext=document.('p').innerHtml="This is really cool!"
   console.log("The DOM has loaded");
 });
  
-console.log(
-  "This console.log() fires when index.js loads - before DOMContentLoaded is triggered"
-);
