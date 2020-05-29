fe743ee452746ab1cd5cd845e5a11aff276faa39
Aya Alassaf <ayafaisal242@yahoo.com>
2020-05-29 21:24:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // Your code goes here
 document.addEventListener("DOMContentLoaded", function() {
-  let p=document.getElementsById('text').innerHTML="This is really cool!"
+  let p=document.getElementsById('text')
+  p.innerHTML="This is really cool!"
  
 });
  
